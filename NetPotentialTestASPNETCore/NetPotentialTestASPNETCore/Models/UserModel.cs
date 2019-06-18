132ebd58f4c667e9f1d9cf849acb7b662c4ba79c
Jorge Calapina <jorgecalapina@gmail.com>
2019-06-18 21:53:31 +1200

    Project creation

---
+++ NetPotentialTestASPNETCore/NetPotentialTestASPNETCore/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace NetPotentialTestASPNETCore.Models
+{
+    public class UserModel
+    {
+        public int[] SelectedTeaIds { get; set; }
+        public IEnumerable<SelectListItem> TeaList { get; set; }
+    }
+}
