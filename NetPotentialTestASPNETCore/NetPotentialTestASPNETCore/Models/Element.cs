eab3386fa2f3016faa46a94d13651f50eaedee41
Jorge Calapina <jorgecalapina@gmail.com>
2019-06-19 00:20:00 +1200

    Commit adding Element class

---
+++ NetPotentialTestASPNETCore/NetPotentialTestASPNETCore/Models/Element.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace NetPotentialTestASPNETCore.Models
+{
+    public class Element
+    {
+        /// <summary>
+        /// Dynamic arrays to mantein the state between request/response
+        /// </summary>
+        public List<int> LeftElementsNow { get; set; }
+        public List<int> RightElementsNow { get; set; }
+
+        /// <summary>
+        /// Arrays to support the elements selected on the listboxes
+        /// </summary>
+        public int[] SelectedLeftItems { get; set; }
+        public int[] SelectedRightItems { get; set; }
+
+        /// <summary>
+        /// List to bind the data to the listboxes
+        /// </summary>
+        public IReadOnlyList<SelectListItem> LeftElements { get; set; }
+        public IReadOnlyList<SelectListItem> RightElements { get; set; }
+    }
+}
